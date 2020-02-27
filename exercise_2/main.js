022d76cf106d18fb421b9b87be81801d046a7abf
David Lyu <lyu.david@yahoo.com>
2020-02-27 15:48:58 -0800

    exercise2 attmpt 2

--- exercise_2/main.js
+++ exercise_2/main.js
@@ -5,18 +5,20 @@ function canNest(arr1, arr2) {
   var arr2Max = null;
 
   for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] < arr[i + 1] && i < arr1.length) {
+    if (arr1Min > arr1[i] && i < arr1.length) {
       arr1Min = arr1[i]
-    } else {
-      arr1Max = arr1[i + 1]
+    }
+    if (arrMax < arr1[i]) {
+      arr1Max = arr1[i]
     }
   }
 
   for (var i = 0; i < arr2.length; i++) {
-    if (arr1[i] < arr[i + 1] && i < arr1.length) {
+    if (arr2Min > arr2[i] && i < arr2.length) {
       arr2Min = arr2[i]
-    } else {
-      arr2Max = arr2[i + 1]
+    }
+    if (arr2Max < arr2[i]) {
+      arr2Max = arr2[i]
     }
   }
   if (arr1Min < arr2Min && arr1Max > arr2Max) {
