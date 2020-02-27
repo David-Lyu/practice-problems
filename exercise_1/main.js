6f46e6bbbb62f484cf94fe3cb56a75cd004b32d3
David Lyu <lyu.david@yahoo.com>
2020-02-26 20:17:42 -0800

    Finished with extra work around

--- exercise_1/main.js
+++ exercise_1/main.js
@@ -2,17 +2,21 @@ decimal = 12;
 
 function binary(decimal){
   var getBinary = [];
-  while(decimal >= 0.5){
-      if(decimal % 2 === 0){
-        getBinary.push(decimal % 2);
-        decimal = decimal/2;
-      }else{
-        getBinary.push(decimal % 2);
-        decimal = decimal / 2 - 0.5;
-      }
+  var stringBinary = "";
+  if (decimal === 0){
+    stringBinary = "0";
+  }else{
+    while(decimal >= 0.5){
+        if(decimal % 2 === 0){
+          getBinary.push(decimal % 2);
+          decimal = decimal/2;
+        }else{
+          getBinary.push(decimal % 2);
+          decimal = decimal / 2 - 0.5;
+        }
+    }
+      var reverseBinary = getBinary.reverse();
+      stringBinary = reverseBinary.join("");
   }
-    var reverseBinary = getBinary.reverse();
-    var stringBinary = reverseBinary.join("");
-
   return stringBinary;
 }
