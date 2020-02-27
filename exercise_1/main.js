877330a3dcf73de81a4b1b22f9a92701c746397f
David Lyu <lyu.david@yahoo.com>
2020-02-26 19:43:01 -0800

    numbers to binary needto flip

--- exercise_1/main.js
+++ exercise_1/main.js
@@ -3,11 +3,12 @@ decimal = 12;
 function binary(decimal){
   var getBinary = "";
   while(decimal >= 0.5){
-      if(decimal%2 === 0){
+      if(decimal % 2 === 0){
         getBinary += decimal % 2;
-        decimal = math.round(decimal/2);
+        decimal = decimal/2;
       }else{
-
+        getBinary += (decimal % 2)
+        decimal = decimal / 2 - 0.5;
       }
   }
   return getBinary
