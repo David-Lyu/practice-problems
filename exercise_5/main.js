a9aaf5ec329e4ab8b1f99de6f60ef534e1214109
David Lyu <lyu.david@yahoo.com>
2020-05-25 09:52:10 -0700

    started the problem

--- exercise_5/main.js
+++ exercise_5/main.js
@@ -1,3 +1,15 @@
   function holiday(x,weekDay,month,yearNumber) {
-
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const monthNum = monthNames.indexOf(month)
+    if(yearNumber%4 != 0) {
+      const MonthDaysYear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    }else {
+      const MonthDaysYear = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    }
+    let dayNum = MonthDaysYear[monthNum]
+    switch weekDay {
+      case Monday:
+    }
   }
+
+  holiday(3,"Wednesday","November",2016)
