5db16ddfcd711fc00ea9f3f6ae168ed3a6b6c6e6
David Lyu <lyu.david@yahoo.com>
2020-05-25 11:51:31 -0700

    commented out q3

--- interviewproblems/q3/main.js
+++ interviewproblems/q3/main.js
@@ -15,36 +15,36 @@
 //doesn't work it doubles, has to do with building of string
 
 
-  const OPEN = '(';
-  const CLOSE = ')';
-  const revereString = (string:string): string => {
-    let output = '';
-    for(let i = string.length -1 ; i >= 0; i--){
-      output += string[i];
-    };
-    return output;
-  };
+//   const OPEN = '(';
+//   const CLOSE = ')';
+//   const revereString = (string:string): string => {
+//     let output = '';
+//     for(let i = string.length -1 ; i >= 0; i--){
+//       output += string[i];
+//     };
+//     return output;
+//   };
 
-const reverseParen = (string:string): string => {
-  const reverse = (string: string):string => {
-    for(let i = 0; i < string.length; i++) { //n^2? n^m
-      if(string[i] === OPEN) {
-        return reverse(string.slice(i + 1)); // because of recursion makes it n^m
-      }
-      if (string[i] === CLOSE) {
-        return reverseString(output);
-      }:
-      output += string[i]
-    }
-    return reverseString(output);
-  }
-  let output = '';
+// const reverseParen = (string:string): string => {
+//   const reverse = (string: string):string => {
+//     for(let i = 0; i < string.length; i++) { //n^2? n^m
+//       if(string[i] === OPEN) {
+//         return reverse(string.slice(i + 1)); // because of recursion makes it n^m
+//       }
+//       if (string[i] === CLOSE) {
+//         return reverseString(output);
+//       }:
+//       output += string[i]
+//     }
+//     return reverseString(output);
+//   }
+//   let output = '';
 
-  for (let i = 0; i < string.length; i++) { //n
-    if(string[i] === OPEN) {
-      output += reverse(string.slice(i + 1));
-    };
-    output += string[i]
-  };
-  return output;
+//   for (let i = 0; i < string.length; i++) { //n
+//     if(string[i] === OPEN) {
+//       output += reverse(string.slice(i + 1));
+//     };
+//     output += string[i]
+//   };
+//   return output;
 };
